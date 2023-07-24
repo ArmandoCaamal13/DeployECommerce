78cb48f9966dbc7f28e3fc69c6167b1fdad23805
ArmandoCaamal13 <ArmandoCaamal13>
2023-07-24 17:45:01 -0500

    Deploy config

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import AdminRoutes from './routes/AdminRoutes';
 import PublicRoutes from './routes/PubllicRoutes';
-import { PayPalScriptProvider } from '@paypal/react-paypal-js'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 
-function App() {
+const App = () => {
 
     return (
         <>
-                <PayPalScriptProvider
-                    options={{
-                        "clientId": "AZlRFeSXl3mfYytrVLkzXK0FgTBW7s_Ok21atTAEjJe5uM-0pRKe729Zb_K7kQkhHqJv2z041UrWK9wl"
-                    }}
-                >
+            <PayPalScriptProvider
+                options={{
+                    "clientId": "AZlRFeSXl3mfYytrVLkzXK0FgTBW7s_Ok21atTAEjJe5uM-0pRKe729Zb_K7kQkhHqJv2z041UrWK9wl"
+                }}
+            >
+                <HashRouter>
                     <Routes>
-                        <Route path='DeployECommerce/admin/*' element={<AdminRoutes />} />
-                        <Route path='DeployECommerce/*' element={<PublicRoutes />} />
+                        <Route path='/DeployECommerce/admin/*' element={<AdminRoutes />} />
+                        <Route path='/DeployECommerce/*' element={<PublicRoutes />} />
                     </Routes>
-                </PayPalScriptProvider>
+                </HashRouter>
+            </PayPalScriptProvider>
         </>
     );
 }
