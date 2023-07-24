8ba7c72a53126fecca2c5125080c5e65b0debbfb
ArmandoCaamal13 <ArmandoCaamal13>
2023-07-24 17:58:27 -0500

    update deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
+
+const router = createHashRouter([{path:'/*', element: <App/>}])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-        <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
     
 )
