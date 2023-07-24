4493781384e477611940fb73e5afa0f5d1087522
ArmandoCaamal13 <ArmandoCaamal13>
2023-07-24 18:08:56 -0500

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AdminRoutes from './routes/AdminRoutes';
 import PublicRoutes from './routes/PubllicRoutes';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
@@ -13,12 +13,10 @@ const App = () => {
                     "clientId": "AZlRFeSXl3mfYytrVLkzXK0FgTBW7s_Ok21atTAEjJe5uM-0pRKe729Zb_K7kQkhHqJv2z041UrWK9wl"
                 }}
             >
-                <HashRouter>
                     <Routes>
                         <Route path='/DeployECommerce/admin/*' element={<AdminRoutes />} />
                         <Route path='/DeployECommerce/*' element={<PublicRoutes />} />
                     </Routes>
-                </HashRouter>
             </PayPalScriptProvider>
         </>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { RouterProvider, createHashRouter } from 'react-router-dom'
+import { HashRouter, RouterProvider, createHashRouter } from 'react-router-dom'
 
 const router = createHashRouter([{path:'/*', element: <App/>}])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <HashRouter>
     <RouterProvider router={router} />
+    </HashRouter>
   </React.StrictMode>
     
 )
